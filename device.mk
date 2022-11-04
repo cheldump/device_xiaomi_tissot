8797ca314114ed9fce855b1579ac189fae120c4e
Mathieu Chartier <mathieuc@google.com>
2022-11-04 07:57:13 +0100

    tissot: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: Ia9ad8661f78650249460e2fb760bd09d8015f824

--- device.mk
+++ device.mk
@@ -357,6 +357,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
+    
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
